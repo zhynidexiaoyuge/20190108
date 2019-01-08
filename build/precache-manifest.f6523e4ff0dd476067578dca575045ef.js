dbfd8e306886c530d31e959d8904dfbc873e7d01
张红宇 <545130548@qq.com>
2019-01-08 10:36:54 +0800

    fixed:relate

--- build/precache-manifest.f6523e4ff0dd476067578dca575045ef.js
+++ build/precache-manifest.f6523e4ff0dd476067578dca575045ef.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "url": "./static/media/logo.5d5d9eef.svg"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "./static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "191fc97c0deac4818a96",
-    "url": "/static/js/main.191fc97c.chunk.js"
+    "url": "./static/js/main.191fc97c.chunk.js"
   },
   {
     "revision": "fa92c112869d3657d47b",
-    "url": "/static/js/1.fa92c112.chunk.js"
+    "url": "./static/js/1.fa92c112.chunk.js"
   },
   {
     "revision": "191fc97c0deac4818a96",
-    "url": "/static/css/main.e2a9939e.chunk.css"
+    "url": "./static/css/main.e2a9939e.chunk.css"
   },
   {
     "revision": "078538dc87e3dbcfd8a2077143dd26e9",
