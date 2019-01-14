0b404e2fd1bd0b15bde7e78d37dd6476d75d35c7
张红宇 <545130548@qq.com>
2019-01-14 18:10:33 +0800

    fixed:aaa

---
+++ src/component/panel/panel.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './panel.css';
+class Panel extends Component{
+    render(){
+        const me = this;
+        return (
+            <div style={{
+                position:'absolute',
+                width:me.props.width||500,
+                height:me.props.height||320,
+                left:me.props.left||20,
+                top:me.props.top||0
+            }} className="panel-box">
+                <div className="panel-border-top"></div>
+                <div className="panel-border-bottom"></div>
+                <h3 className="panel-title">{this.props.title}</h3>
+                <div className="panel-children">
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
+export default Panel;
--- src/page/home-page/HomePage.js
+++ src/page/home-page/HomePage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './homePage.css';
+import Panel from '../../component/panel/panel'
 class HomePage extends Component{
     render(){
         return (
             <div style={{position:'relative'}} className="homePage-Box">
-                homePage
+                <Panel title={'测试标题'} top={20}>aaaaa</Panel>
+                <Panel title={'测试标题'} top={360}>aaaaa</Panel>
+                <Panel title={'测试标题'} top={700}>aaaaa</Panel>
             </div>
         )
     }
